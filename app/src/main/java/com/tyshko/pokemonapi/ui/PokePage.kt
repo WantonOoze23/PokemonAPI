b16f27c02d1f9624acd6b4eb630984dadee4afcd
Daniil <mine16978@gmail.com>
2025-03-01 12:23:57 +0200

    visual part redone

--- app/src/main/java/com/tyshko/pokemonapi/ui/PokePage.kt
+++ app/src/main/java/com/tyshko/pokemonapi/ui/PokePage.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
