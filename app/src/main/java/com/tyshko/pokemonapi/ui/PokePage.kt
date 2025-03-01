789cec6f0d7c034e3b365bad8f84bf727c82d972
Daniil <mine16978@gmail.com>
2025-03-01 15:43:50 +0200

    visual part redone

--- app/src/main/java/com/tyshko/pokemonapi/ui/PokePage.kt
+++ app/src/main/java/com/tyshko/pokemonapi/ui/PokePage.kt
@@ -33,8 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import coil.compose.AsyncImage
 
 import com.tyshko.pokemonapi.model.Pokemon
